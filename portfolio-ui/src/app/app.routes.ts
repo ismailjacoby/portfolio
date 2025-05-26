5cdacc77b1c1c5fa56b7020f27aa1a863a53b928
Ismail <ismail.jacoby@gmail.com>
2025-05-26 17:01:19 +0200

    Frontend: AuthService, Environments & Refactoring

--- portfolio-ui/src/app/app.routes.ts
+++ portfolio-ui/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
-import {LoginComponent} from './components/auth/login/login.component';
+import {LoginComponent} from './components/admin/login/login.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
