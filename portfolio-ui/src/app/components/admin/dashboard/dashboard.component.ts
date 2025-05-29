8f280b77c23c59da14813c201099f9975cd80a7c
Ismail <ismail.jacoby@gmail.com>
2025-05-29 13:15:26 +0200

    Frontend: Updated Dashboard to be Reactive based on what tab I choose

--- portfolio-ui/src/app/components/admin/dashboard/dashboard.component.ts
+++ portfolio-ui/src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {NgClass, TitleCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [
+    NgClass,
+    TitleCasePipe
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  selectedTab: 'project' | 'experience' | 'skill' = 'project';
+
+
 
 }
